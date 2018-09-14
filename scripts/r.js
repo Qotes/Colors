83dd0c8f176310343fbf5a6de2bcc392f7c7a3a8
sy <somarl@live.com>
2018-09-14 10:42:44 +0800

    feat(fonts): simkai partially compressed with font-spider

--- scripts/r.js
+++ scripts/r.js
@@ -1,3 +1,17 @@
+/**
+ * @desc how to compress the fonts ?
+ *   // check the commit log, download the simkai.ttf
+ *   // change the scss, and make sure colors has been collected
+ *   npm run build
+ *   node scripts/p.js
+ *   cp build/index.html build/kanji.html
+ *   // copy the characters in kanji.json to build/kanji.html
+ *   font-spider build/kanji.html
+ *   // cp build/media/font-spider/simkai.*.woff src/fonts/simkai.woff
+ *   // set back the scss
+ *   npm run build
+ */
+
 const fs = require('fs')
 const path = require('path')
 
--- src/index.tsx
+++ src/index.tsx
@@ -2,8 +2,6 @@ import * as React from 'react'
 import * as ReactDOM from 'react-dom'
 import App from './App'
 import './css/index.css'
-import './texture.png'
-import './fonts'
 import registerServiceWorker from './registerServiceWorker'
 
 
