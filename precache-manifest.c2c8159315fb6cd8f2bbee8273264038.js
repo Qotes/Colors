2d12ef06fb2c906606fd15ab7e0a9f4de0454572
sy <somarl@live.com>
2019-04-27 15:18:25 +0800

    Updates

---
+++ precache-manifest.c2c8159315fb6cd8f2bbee8273264038.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0e55eb6d62cdaba895d7b5b346a35779",
+    "url": "/Colors/index.html"
+  },
+  {
+    "revision": "b9f8e07d03f69138e68e",
+    "url": "/Colors/static/css/main.9513a261.chunk.css"
+  },
+  {
+    "revision": "1cf978a3e443d4fdc3a0",
+    "url": "/Colors/static/js/2.ea15365d.chunk.js"
+  },
+  {
+    "revision": "b9f8e07d03f69138e68e",
+    "url": "/Colors/static/js/main.03b474e6.chunk.js"
+  },
+  {
+    "revision": "f619ed9571621b7a4df2",
+    "url": "/Colors/static/js/runtime~main.b4ebc45b.js"
+  },
+  {
+    "revision": "c398a2f938abd3502ef379e4b113af70",
+    "url": "/Colors/static/media/OpenSans-Light.c398a2f9.woff"
+  },
+  {
+    "revision": "3e0736a3a2a337e0e4322a92b86a13f3",
+    "url": "/Colors/static/media/bebasneue.3e0736a3.woff2"
+  },
+  {
+    "revision": "c902a513b8aee8ef6ffa71c92756b865",
+    "url": "/Colors/static/media/bebasneue.c902a513.woff"
+  },
+  {
+    "revision": "43a46dc51bc1abd9dd8f235047576e3a",
+    "url": "/Colors/static/media/ebgaramond.43a46dc5.woff2"
+  },
+  {
+    "revision": "66be320845f4fccb6fc8a30e50027890",
+    "url": "/Colors/static/media/ebgaramond.66be3208.woff"
+  },
+  {
+    "revision": "58c52d4d7fa92445c9fc1ae99c5163ba",
+    "url": "/Colors/static/media/simkai.58c52d4d.woff"
+  }
+]);
